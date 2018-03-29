f6f3dcc9b844abfa258ee9f28071c9b507cff3b5
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-03-29 15:28:44 +0300

    Dynamic features: clicks count, color changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template lang="pug">
   div
-    p And this is a Vue component!
+    span.hash {{ color }}
+    p Number of clicks: {{ number }}
+    button(@click="handleClick", :style="{'backgroundColor': color}") Click me!
+    button(@click="reset") Reset
 </template>
 
 <script lang="ts">
   import { Component, Vue }  from 'vue-property-decorator';
 
   @Component({
-    name: 'App'
+    name: 'App',
+    computed: {
+      color() {
+        const hash = (Math.random() * this.number).toString(16);
+        return this.number === 0 ? '#fff' : '#' + hash.substr(hash.indexOf('.') + 1, 6);
+      }
+    }
   })
-  export default class App extends Vue {}
+  export default class App extends Vue {
+
+    public number: number = 0;
+    public color: string; 
+    
+    private handleClick(): void {
+      this.number++;
+    }
+
+    private reset() {
+      this.number = 0;
+    }
+  }
 
 </script>
 
@@ -18,7 +39,8 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 0 auto;
+    flex-direction: column;
+    margin: 20px auto;
     width: 250px;
     height: 250px;
     color: rgb(192, 118, 8);
@@ -26,8 +48,20 @@
     border-radius: 45%;
 
     p {
-      font: italic 20px/200% Georgia, sans-serif;
+      font: 20px/200% Georgia, sans-serif;
       color: inherit;
+    }
+
+    button {
+      display: block;
+      margin: 10px auto;
+      border: 1px solid #888;
+      padding: 5px 10px;
+      border-radius: 4px;
+    }
+
+    .hash {
+      font-family: monospace;
     }
   }
 </style>
